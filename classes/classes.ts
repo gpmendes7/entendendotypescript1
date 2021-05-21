2f58d53008023da5de4bf3256eed84dd13530c98
gabriel7mendes <andrewsseven@gmail.com>
2021-05-21 15:08:40 -0300

    classes

---
+++ classes/classes.ts
@@ -0,0 +1,36 @@
+class Data {
+    public dia: number
+    mes: number
+    ano: number
+
+    constructor(dia: number = 1, mes: number = 1,
+        ano: number = 1970) {
+        this.dia = dia
+        this.mes = mes
+        this.ano = ano
+    }
+}
+
+const aniversario = new Data(3, 11, 1991)
+aniversario.dia = 4
+console.log(aniversario.dia)
+console.log(aniversario)
+
+const casamento = new Data
+casamento.ano = 2017
+console.log(casamento)
+
+class DataEsperta {
+    constructor(public dia: number = 1, public mes: number = 1,
+        public ano: number = 1970) {
+    }
+}
+
+const aniversarioEsperto = new DataEsperta(3, 11, 1991)
+aniversarioEsperto.dia = 4
+console.log(aniversarioEsperto.dia)
+console.log(aniversarioEsperto)
+
+const casamentoEsperto = new DataEsperta
+casamentoEsperto.ano = 2017
+console.log(casamentoEsperto)
