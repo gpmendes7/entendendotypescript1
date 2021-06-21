a921a2f63e8da817ad268842ddf71e794b4f8d85
gabriel7mendes <andrewsseven@gmail.com>
2021-06-21 17:48:51 -0300

    generics

--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -18,3 +18,22 @@ const avaliacoes: Array<number> = [10, 9.3, 7.7]
 avaliacoes.push(8.4)
 // avaliacoes.push('5.5')
 console.log(avaliacoes)
+
+function imprimir<T>(args: T[]) {
+  args.forEach(elemento => console.log(elemento));
+}
+
+imprimir([1, 2, 3])
+imprimir<number>([1, 2, 3])
+imprimir<string>(['Ana', 'Bia', 'Carlos'])
+imprimir<{ nome: string, idade: number}>([
+  { nome: 'Fulano', idade: 22 },
+  { nome: 'Cicrano', idade: 23 },
+  { nome: 'Beltrano', idade: 24 }
+])
+type Aluno = { nome: string, idade: number }
+imprimir<Aluno>([
+  { nome: 'Fulano', idade: 22 },
+  { nome: 'Cicrano', idade: 23 },
+  { nome: 'Beltrano', idade: 24 }
+])
