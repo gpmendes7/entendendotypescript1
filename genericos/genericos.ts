68236a7319ebf7db1de7f635384a65444893485d
gabriel7mendes <andrewsseven@gmail.com>
2021-06-23 18:04:35 -0300

    generics

--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -124,3 +124,42 @@ const novaFila = new Fila<number>(1,2,3)
 novaFila.imprimir()
 
 // const outraFila = new Fila<boolean>(true, false)
+
+type Par<C. V> = { chave: C, valor: V}
+
+class Mapa<C, V> {
+  itens: Array<Par<C,V>> = new Array<Par<C,V>>()
+
+  obter(chave: C): Par<C,V> | null {
+    const resultado = this.itens.filter(i => i.chave === chave)
+    return resultado ? resultado[0] : null
+  }
+
+  colocar(par: Par<C, V>) {
+    const encontrado = this.obter(par.chave)
+    if(encontrado) {
+      encontrado.valor = par.valor
+    } else {
+      this.itens.push(par)
+    }
+  }
+
+  limpar() {
+    this.itens = new Array<Par<C,V>>()
+  }
+
+  imprimir() {
+    console.log(this.itens)
+  }
+}
+
+const mapa = new Mapa<number, string>()
+mapa.colocar({ chave: 1, valor: 'Pedro'})
+mapa.colocar({ chave: 2, valor: 'Rebeca'})
+mapa.colocar({ chave: 3, valor: 'Maria'})
+mapa.colocar({ chave: 1, valor: 'Gustavo'})
+
+console.log(mapa.obter(2))
+mapa.imprimir()
+mapa.limpar()
+mapa.imprimir()
